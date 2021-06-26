6ddbf5c381a2ab0308c785f62252005e0795affc
Tatsuo Nakajyo <tnak@nekonaq.com>
2021-06-27 05:05:24 +0900

    Release 1.0.2106

---
+++ manifests/init.pp
@@ -0,0 +1,19 @@
+class stdapp {
+  include std
+
+  $base_dir = '/app'
+
+  $systemd_unit_perm = {
+    mode => '644',
+    owner => 'root',
+    group => 'root',
+  }
+
+  file { stdapp::base_dir:
+    path => $base_dir,
+    ensure => directory,
+    mode => '755',
+    owner => 'root',
+    group => 'wheel',
+  }
+}
